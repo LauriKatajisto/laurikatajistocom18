25a9ddf39488765f15f7fbdca2f59bfff420329a
LauriKatajisto <lauri.katajisto@gmail.com>
2018-03-21 23:54:13 +0200

    More helpers.

--- test/helpers.js
+++ test/helpers.js
@@ -1,3 +1,12 @@
 import { expect } from 'chai';
+import { mount, render, shallow, configure, debug } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 global.expect = expect;
+
+global.mount = mount;
+global.render = render;
+global.shallow = shallow;
+global.debug = debug;
